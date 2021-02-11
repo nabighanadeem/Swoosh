340a40edec6fb198e76fb967e3adee6bc965c60e
nabighanadeem <nadeemnabigha44@gmail.com>
2021-02-11 16:55:56 +0500

    lesson-04

--- app/src/main/java/com/nabigha/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/nabigha/swoosh/LeagueActivity.kt
@@ -1,11 +1,18 @@
 package com.nabigha.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View){
+        val skill_Activity = Intent(this, skillActivity::class.java)
+        startActivity(skill_Activity)
     }
 }
---
+++ app/src/main/java/com/nabigha/swoosh/skillActivity.kt
@@ -0,0 +1,11 @@
+package com.nabigha.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class skillActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
